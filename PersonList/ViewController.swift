0c832c504961d9da23d81612e2c1083989ab93da
Антон Редкозубов <uacreez@gmail.com>
2020-02-19 22:06:41 +0400

    Initial Commit

---
+++ PersonList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PersonList
+//
+//  Created by Anton Redkozubov on 19.02.2020.
+//  Copyright © 2020 Anton Redkozubov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
